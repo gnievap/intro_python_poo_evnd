62e66834e93930be004e2b8773aa9f1f26a6fe33
Gaby Nieva <gaby.nieva@outlook.com>
2026-05-13 10:23:29 -0600

    Sumando dos números

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 def main():
     print("Hello, World!")
     print("Hola de nuevo, Mundo!")
+    resultado = suma()
+    print(f"La suma de los dos números es: {resultado}")
+
+def suma():
+    num1 = int(input("Ingrese el primer número: "))
+    num2 = int(input("Ingrese el segundo número: "))
+    return num1 + num2
 
 if __name__ == "__main__":
     main()
